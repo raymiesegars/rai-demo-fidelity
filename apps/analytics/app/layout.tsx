b921677f54b63a398d448fbc7cfa02ca1a6cdecb
Raymie Segars <raymievsegarsiv@gmail.com>
2026-07-23 16:18:35 -0500

    visual updates

--- apps/analytics/app/layout.tsx
+++ apps/analytics/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link
-          href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,400;0,9..40,560;0,9..40,650;1,9..40,400&family=Fraunces:opsz,wght@9..144,500;9..144,560&display=swap"
+          href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,400;0,9..40,500;0,9..40,600;0,9..40,700;1,9..40,400&family=Fraunces:opsz,wght@9..144,500;9..144,600;9..144,700&display=swap"
           rel="stylesheet"
         />
       </head>
